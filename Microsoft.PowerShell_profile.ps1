5519389b679e4d248870b84869adc06f00c9e465
Johno <riscostm@me.com>
2024-01-21 16:16:17 +0000

    update paths

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -15,7 +15,7 @@
 
 #check for updates
 try{
-    $url = "https://raw.githubusercontent.com/riscostm/powershell-bling/raw/main/Microsoft.PowerShell_profile.ps1"
+    $url = "https://github.com/riscostm/powershell-bling/blob/921c3f86280ff27ea1c74da71cee6c9e411abcfd/Microsoft.PowerShell_profile.ps1"
     $oldhash = Get-FileHash $PROFILE
     Invoke-RestMethod $url -OutFile "$env:temp/Microsoft.PowerShell_profile.ps1"
     $newhash = Get-FileHash "$env:temp/Microsoft.PowerShell_profile.ps1"
